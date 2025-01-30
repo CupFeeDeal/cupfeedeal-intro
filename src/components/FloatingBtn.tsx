bd5973e12535db73cddbf5b29a94d100782d1ea1
s-uxun <serenasol@ewha.ac.kr>
2025-01-31 08:59:25 +0900

    fix: 플로팅 버튼 위치 수정

--- src/components/FloatingBtn.tsx
+++ src/components/FloatingBtn.tsx
@@ -7,8 +7,8 @@ const FloatingBtn = () => {
       target="_blank"
       rel="noopener noreferrer"
     >
-      <div className="fixed max-w-[440px] mx-auto px-5 bottom-20 w-full ">
-        <div className="px-6 py-[14px] drop-shadow-lg bg-Main_Blue rounded-xl Body_1_bold text-white text-center animate-float">
+      <div className="fixed max-w-[440px] mx-auto px-5 bottom-10 w-full ">
+        <div className=" py-[0.88rem] drop-shadow-lg bg-Main_Blue rounded-xl Body_1_bold text-white text-center animate-float">
           커피딜 바로가기
         </div>
       </div>
